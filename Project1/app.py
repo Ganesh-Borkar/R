6eb5642d3c4658e230dd25156d0a7b48393911d3
Ganesh Borkar <ganeshborkar107@gmail.com>
2024-02-11 08:28:10 +0530

    Initial commit

---
+++ Project1/app.py
@@ -0,0 +1,54 @@
+from flask import Flask,request,render_template
+import joblib
+
+model = joblib.load(open('../../Project1/Models/KMEANS.pkl'),'rb')
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def welcome():
+
+    return "Welcome !!!"
+
+@app.route("/index",methods=["GET"])
+def index():
+    return "<h1>Welcome to the Index page<h1>"
+
+
+@app.route("/predict",methods=["GET","POST"])
+def predict():
+    if request.method=="GET":
+
+        return render_template("predict.html")
+    
+    else:
+        
+        T1=float(request.form["T1"])
+        T2=float(request.form["T2"])
+        T3=float(request.form["T3"])
+        T4=float(request.form["T4"])
+        T5=float(request.form["T5"])
+        T6=float(request.form["T6"])
+        T7=float(request.form["T7"])
+        T8=float(request.form["T8"])
+        T9=float(request.form["T9"])
+        T10 = float(request.form["T10"])
+        T11 = float(request.form["T11"])
+        T12 = float(request.form["T12"])
+        T13 = float(request.form["T13"])
+        T14 = float(request.form["T14"])
+        T15 = float(request.form["T15"])
+        T16 = float(request.form["T16"])
+        T17 = float(request.form["T17"])
+        T18 = float(request.form["T18"])
+
+        Prediction = model.predict([[T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15,T16,T17,T18]])
+
+        return render_template("predict.html",Target=Prediction)
+
+        
+    
+
+
+if __name__=='__main__':
+    app.run(debug=True)
